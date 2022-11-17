ab60d09279096b22754ee0a64733032e36a552e8
Grey Rabbitty <GreyRabbitty@gmail.com>
2022-11-17 23:20:08 +0600

    added my name
    with the link

--- interface/pages/index.js
+++ interface/pages/index.js
@@ -335,7 +335,7 @@ export default function Home() {
         </div>
       </div>
 
-      <footer className={styles.footer}>Made with &#10084; by Your Name</footer>
+      <footer className={styles.footer}>Made with &#10084; by <a href="https://www.linkedin.com/in/meharab124/">Meharab</a></footer>
     </div>
   );
 }
